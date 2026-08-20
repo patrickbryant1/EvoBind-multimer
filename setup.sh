f06232de8c5025b6d18c6bd9ac5ec8750f4534d7
DiandraDaum <158558086+DiandraDaum@users.noreply.github.com>
2026-08-20 15:43:35 +0200

    Refactor setup.sh for better parameter handlingupdated setup
    Updated setup script to improve parameter downloading and environment setup.

--- setup.sh
+++ setup.sh
@@ -1,28 +1,39 @@
-### Download AF2 parameters
-cd ./src
-mkdir params
-cd params
-wget https://storage.googleapis.com/alphafold/alphafold_params_2021-07-14.tar
+#!/bin/bash
+
+# Download AF2 parameters
+mkdir -p ./src/params
+cd ./src/params
+echo "Downloading AlphaFold parameters..."
+wget -c https://storage.googleapis.com/alphafold/alphafold_params_2021-07-14.tar
 tar -xf alphafold_params_2021-07-14.tar
 rm alphafold_params_2021-07-14.tar
 cd ../../
 
 #Python packages
-conda env create -f environment.yml
-wait
-conda activate evobind
-pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
-conda deactivate
+CONDA_CMD=$(command -v mamba || command -v conda) # pick mamba if installed, otherwise default to conda
+echo "Creating environment using $CONDA_CMD..."
+$CONDA_CMD env create -f environment.yml 2>/dev/null || $CONDA_CMD env update -f environment.yml --prune
+CONDA_BASE=$($CONDA_CMD info --base)
+source "$CONDA_BASE/etc/profile.d/conda.sh"
+conda activate EBM
 
-## HHblits (requires cmake)
+## HHblits
+echo "Setting up HH-suite..."
 git clone https://github.com/soedinglab/hh-suite.git
 mkdir -p hh-suite/build && cd hh-suite/build
-cmake -DCMAKE_INSTALL_PREFIX=. ..
-make -j 4 && make install
+cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+      -DCMAKE_INSTALL_PREFIX=. ..
+make -j 4
+make install
 cd ../..
+conda deactivate
 
-cd data
 ### Download uniclust30_2018_08
-wget http://wwwuser.gwdg.de/~compbiol/uniclust/2018_08/uniclust30_2018_08_hhsuite.tar.gz --no-check-certificate
+echo "Downloading Uniclust30 database..."
+cd data
+wget -c https://wwwuser.gwdg.de/~compbiol/uniclust/2018_08/uniclust30_2018_08_hhsuite.tar.gz
 tar -zxvf uniclust30_2018_08_hhsuite.tar.gz
-rm uniclust30_2018_08_hhsuite.tar.gz
+rm -f uniclust30_2018_08_hhsuite.tar.gz
+cd ..
+
+echo "Setup completed!"
